eecf61be1d83da76a8b85d310f659d045c0c6b06
CS-Aditya-Rawat <adityainferno4812@gmail.com>
2023-09-27 17:08:46 +0530

    Removed Extra Files

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Notes
-
-
-class NoteSerializer(ModelSerializer):
-    class Meta:
-        model = Notes
-        fields = "__all__"
