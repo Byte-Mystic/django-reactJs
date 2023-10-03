3fc3d1f28f853d737e87aeeb07971108aabc7f46
CS-Aditya-Rawat <adityainferno4812@gmail.com>
2023-10-03 18:14:48 +0530

    Change StaticFiles

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "mynotes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "frontend/build"],
+        "DIRS": [BASE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [BASE_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
